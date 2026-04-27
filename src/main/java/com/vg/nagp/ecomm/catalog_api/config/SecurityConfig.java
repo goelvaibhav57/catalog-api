989c05fa832484f777239540e366b7798e3fb501
goelvaibhav57 <60250579+goelvaibhav57@users.noreply.github.com>
2026-04-27 19:29:25 +0530

    Update allowed origins for CORS configuration

--- src/main/java/com/vg/nagp/ecomm/catalog_api/config/SecurityConfig.java
+++ src/main/java/com/vg/nagp/ecomm/catalog_api/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200", "https://your-s3-domain"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://my-ecomm-frontend-app.s3-website-us-east-1.amazonaws.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
